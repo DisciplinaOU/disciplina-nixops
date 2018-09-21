0cca588417c9f4de0ade90e303a859197c44f094
Yegor Timoshenko <yegortimoshenko@riseup.net>
2018-09-21 21:41:46 +0000

    Initialize

---
+++ profiles/darwin-builder.nix
@@ -0,0 +1,54 @@
+{ config, pkgs, ... }:
+
+let
+  # Impure, requires MacStadium and some setup:
+  # $ ssh-keygen -t ed25519
+  secrets = "/Users/administrator";
+in
+
+{
+  networking.hostName = "disciplina-darwin-builder";
+
+  nix = {
+    binaryCaches = [
+      "https://disciplina.cachix.org"
+    ];
+
+    binaryCachePublicKeys = [
+      "disciplina.cachix.org-1:zDeIFV5cu22v04EUuRITz/rYxpBCGKY82x0mIyEYjxE="
+    ];
+
+    package = pkgs.nix;
+
+    # Doesn't work because buildkite-agent3 doesn't build due to Go impurely
+    # depending on CoreFoundation framework. Should be fixed in our Nixpkgs.
+    # useSandbox = true;
+  };
+
+  programs.bash.enable = true;
+
+  services = {
+    buildkite-agent = {
+      enable = true;
+
+      meta-data = "system=${builtins.currentSystem}";
+
+      openssh = {
+        privateKeyPath = "${secrets}/.ssh/id_ed25519";
+        publicKeyPath = "${secrets}/.ssh/id_ed25519.pub";
+      };
+
+      package = pkgs.buildkite-agent3;
+      runtimePackages = [ pkgs.nix ];
+      tokenPath = "${secrets}/buildkite-token";
+    };
+
+    nix-daemon.enable = true;
+  };
+
+  system.activationScripts.postActiation.text = ''
+    mkdir -p ${config.users.users.buildkite-agent.home}
+    chown buildkite-agent:buildkite-agent ${config.users.users.buildkite-agent.home}
+    chmod 770 ${config.users.users.buildkite-agent.home}
+  '';
+}
