d33a47860711c84b6a6d57ca86518b297d1eac01
Yegor Timoshenko <yegortimoshenko@riseup.net>
2018-09-27 19:37:18 +0000

    aws-rotate-key: apply patch instead of custom src

--- pkgs/default.nix
+++ pkgs/default.nix
@@ -6,10 +6,10 @@ in
 
 {
   aws-rotate-key = previous.aws-rotate-key.overrideAttrs (super: {
-    src = fetchGit {
-      url = "https://github.com/serokell/aws-rotate-key";
-      rev = "5606b4c2e2a395560618126a3c1e2afaf1c0d2f3";
-    };
+    patches = (super.patches or []) ++ [(final.fetchpatch {
+      url = https://github.com/serokell/aws-rotate-key/commit/5606b4c2e2a395560618126a3c1e2afaf1c0d2f3.patch;
+      sha256 = "0g120qbdg92lf57jw8wwc515gk4v4k0z569c33mb25rzkarlsjrd";
+    })];
   });
 
   nix-with-cachix = callPackage ./nix-with-cachix {};
