5c5d625885c90908d2d96051e51334a8e6a49483
Yegor Timoshenko <yegortimoshenko@riseup.net>
2018-09-28 23:52:24 +0000

    nginx: apply ETag patch (see NixOS/nixpkgs#25485)

--- pkgs/default.nix
+++ pkgs/default.nix
@@ -12,6 +12,13 @@ in
     })];
   });
 
+  nginxStable = previous.nginxStable.overrideAttrs (super: {
+    patches = (super.patches or []) ++ [(fetchpatch {
+      url = https://gitlab.com/yegortimoshenko/patches/raw/166f4fa4d90a7dca5c9eeeda2aaf2f1d4b841e87/nginx/nix-etag-1.15.4.patch;
+      sha256 = "09wi3zgizr0vgy24pkqbswr1318yxihr7jdjbl29q44glsjqg5rb";
+    })];
+  });
+
   nix-with-cachix = callPackage ./nix-with-cachix {};
 
   nixops = callPackage ./nixops {
