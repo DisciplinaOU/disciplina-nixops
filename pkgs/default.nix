b67939dbd8b294da33822637bbeed14984f70742
Yegor Timoshenko <yegortimoshenko@riseup.net>
2018-09-28 23:51:51 +0000

    aws-rotate-key: update patch src

--- pkgs/default.nix
+++ pkgs/default.nix
@@ -1,13 +1,13 @@
 final: previous:
 
 let
-  inherit (final) callPackage;
+  inherit (final) callPackage fetchpatch;
 in
 
 {
   aws-rotate-key = previous.aws-rotate-key.overrideAttrs (super: {
-    patches = (super.patches or []) ++ [(final.fetchpatch {
-      url = https://github.com/serokell/aws-rotate-key/commit/5606b4c2e2a395560618126a3c1e2afaf1c0d2f3.patch;
+    patches = (super.patches or []) ++ [(fetchpatch {
+      url = https://github.com/Fullscreen/aws-rotate-key/commit/5606b4c2e2a395560618126a3c1e2afaf1c0d2f3.patch;
       sha256 = "0vpi10p6q2yz2j5j6ird9w4bcr8bagh1c4bqm2v2rmymbbwx46ai";
     })];
   });
