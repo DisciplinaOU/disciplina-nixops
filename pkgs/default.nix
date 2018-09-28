751179a79ac0807c5c7db9881449242bd183cf2d
Christian Höppner <christian.hoppner@serokell.io>
2018-09-28 13:37:32 +0100

    Fix SHA256 for aws-rotate-key

--- pkgs/default.nix
+++ pkgs/default.nix
@@ -8,7 +8,7 @@ in
   aws-rotate-key = previous.aws-rotate-key.overrideAttrs (super: {
     patches = (super.patches or []) ++ [(final.fetchpatch {
       url = https://github.com/serokell/aws-rotate-key/commit/5606b4c2e2a395560618126a3c1e2afaf1c0d2f3.patch;
-      sha256 = "0g120qbdg92lf57jw8wwc515gk4v4k0z569c33mb25rzkarlsjrd";
+      sha256 = "0vpi10p6q2yz2j5j6ird9w4bcr8bagh1c4bqm2v2rmymbbwx46ai";
     })];
   });
 
