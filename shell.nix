9b4cbb9d24a231a53cc4705f0a318f1ba39a9983
Christian Höppner <christian.hoppner@serokell.io>
2018-12-18 19:45:42 +0000

    Fix build issues
    * Repin closure and nixpkgs to fix eval
    * Repin nixops to fix eval
    * Remove aws-rotate-key, it wasn't being used and doesn't build
    * Do not build documentation

--- shell.nix
+++ shell.nix
@@ -4,7 +4,7 @@ with pkgs;
 
 stdenv.mkDerivation {
   name = "disciplina-nixops";
-  nativeBuildInputs = [ aws-rotate-key git-crypt nixops sqlite ];
+  nativeBuildInputs = [ git-crypt nixops sqlite ];
 
   AWS_ACCESS_KEY_ID = "default";
   AWS_SHARED_CREDENTIALS_FILE = "${toString ./.}/keys/${env}/aws-credentials";
