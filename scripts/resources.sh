ba9f370d3bd83725b4c13bc6b52b90544626acdd
Christian Höppner <christian.hoppner@serokell.io>
2019-01-03 12:35:12 +0000

    resources.sh: read from file into deployment

--- scripts/resources.sh
+++ scripts/resources.sh
@@ -24,22 +24,31 @@ DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null && pwd )"
 SFILE="$DIR/../state/deployer.nixops"
 
 json="$(nixops export -s "$SFILE" -d "$from")"
-shared_vpc_id=$(echo "$json" | jq -r ".[].resources[\"shared-vpc\"].vpcId")
+shared_vpc_id=$(echo "$json"   | jq -r ".[].resources[\"shared-vpc\"].vpcId")
 shared_vpc_cidr=$(echo "$json" | jq -r ".[].resources[\"shared-vpc\"].cidrBlock")
-route_table_id=$(echo "$json" | jq -r ".[].resources[\"route-table\"].routeTableId")
+route_table_id=$(echo "$json"  | jq -r ".[].resources[\"route-table\"].routeTableId")
 
-if [[ $mode == local ]]; then
-    nixops set-args -d "$to" --argstr vpcId "$shared_vpc_id"
-    nixops set-args -d "$to" --argstr vpcCidr "$shared_vpc_cidr"
-    nixops set-args -d "$to" --argstr routeTableId "$route_table_id"
+if [[ $do == write ]]; then
+    if [[ $mode == local ]]; then
+        # Write IDs to local deployment
+        nixops set-args -d "$to" --argstr vpcId        "$shared_vpc_id"
+        nixops set-args -d "$to" --argstr vpcCidr      "$shared_vpc_cidr"
+        nixops set-args -d "$to" --argstr routeTableId "$route_table_id"
 
-elif [[ $mode == remote ]]; then
-    echo "$shared_vpc_id" >| shared_vpc_id
-    echo "$shared_vpc_cidr" >| shared_vpc_cidr
-    echo "$route_table_id" >| route_table_id
+    elif [[ $mode == remote ]]; then
+        # Write IDs to files on server
+        echo "$shared_vpc_id" >| shared_vpc_id
+        echo "$shared_vpc_cidr" >| shared_vpc_cidr
+        echo "$route_table_id" >| route_table_id
 
-    nixops scp -s "$SFILE" -d "$from" builder shared_vpc_id /var/lib/nixops/shared_vpc_id
-    nixops scp -s "$SFILE" -d "$from" builder shared_vpc_cidr /var/lib/nixops/shared_vpc_cidr
-    nixops scp -s "$SFILE" -d "$from" builder route_table_id /var/lib/nixops/route_table_id
+        nixops scp -s "$SFILE" -d "$from" builder shared_vpc_id   /var/lib/nixops/shared_vpc_id
+        nixops scp -s "$SFILE" -d "$from" builder shared_vpc_cidr /var/lib/nixops/shared_vpc_cidr
+        nixops scp -s "$SFILE" -d "$from" builder route_table_id  /var/lib/nixops/route_table_id
+    fi
 
+elif [[ $do == write ]]; then
+    # Read existing IDs from files and write to deployment
+    nixops set-args -d "$to" --argstr vpcId        "$(cat /var/lib/nixops/shared_vpc_id)"
+    nixops set-args -d "$to" --argstr vpcCidr      "$(cat /var/lib/nixops/shared_vpc_cidr)"
+    nixops set-args -d "$to" --argstr routeTableId "$(cat /var/lib/nixops/route_table_id)"
 fi
