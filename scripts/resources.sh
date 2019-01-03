82d86b70710776fd7e60d2cad0861142b094dd64
Christian Höppner <christian.hoppner@serokell.io>
2019-01-03 12:23:27 +0000

    resources.sh: remote mode
    Remote mode puts the resource IDs in files on the corresponding builder node.

--- scripts/resources.sh
+++ scripts/resources.sh
@@ -18,17 +18,28 @@ set -euo pipefail
 
 from=${1:-staging}
 to=${2:-disciplina}
+mode=${3:-local}
 
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null && pwd )"
 SFILE="$DIR/../state/deployer.nixops"
 
 json="$(nixops export -s "$SFILE" -d "$from")"
-
 shared_vpc_id=$(echo "$json" | jq -r ".[].resources[\"shared-vpc\"].vpcId")
 shared_vpc_cidr=$(echo "$json" | jq -r ".[].resources[\"shared-vpc\"].cidrBlock")
 route_table_id=$(echo "$json" | jq -r ".[].resources[\"route-table\"].routeTableId")
 
-nixops set-args -d "$to" --argstr vpcId "$shared_vpc_id"
-nixops set-args -d "$to" --argstr vpcCidr "$shared_vpc_cidr"
-nixops set-args -d "$to" --argstr routeTableId "$route_table_id"
+if [[ $mode == local ]]; then
+    nixops set-args -d "$to" --argstr vpcId "$shared_vpc_id"
+    nixops set-args -d "$to" --argstr vpcCidr "$shared_vpc_cidr"
+    nixops set-args -d "$to" --argstr routeTableId "$route_table_id"
 
+elif [[ $mode == remote ]]; then
+    echo "$shared_vpc_id" >| shared_vpc_id
+    echo "$shared_vpc_cidr" >| shared_vpc_cidr
+    echo "$route_table_id" >| route_table_id
+
+    nixops scp -s "$SFILE" -d "$from" builder shared_vpc_id /var/lib/nixops/shared_vpc_id
+    nixops scp -s "$SFILE" -d "$from" builder shared_vpc_cidr /var/lib/nixops/shared_vpc_cidr
+    nixops scp -s "$SFILE" -d "$from" builder route_table_id /var/lib/nixops/route_table_id
+
+fi
