37e2949d7f3025d1469802d342f7054b036d6d6a
Lars Jellema <lars.jellema@gmail.com>
2019-01-16 22:29:31 +0100

    Remove unused parts of scripts/resources.sh

--- scripts/resources.sh
+++ scripts/resources.sh
@@ -22,35 +22,13 @@ set -euo pipefail
 
 from=${1:-staging}
 to=${2:-disciplina}
-do=${3:-write}
-mode=${4:-local}
 
 json="$(nixops export -d "$from")"
 shared_vpc_id=$(echo "$json"   | jq -r ".[].resources[\"shared-vpc\"].vpcId")
 shared_vpc_cidr=$(echo "$json" | jq -r ".[].resources[\"shared-vpc\"].cidrBlock")
 route_table_id=$(echo "$json"  | jq -r ".[].resources[\"route-table\"].routeTableId")
 
-if [[ $do == write ]]; then
-    if [[ $mode == local ]]; then
-        # Write IDs to local deployment
-        nixops set-args -d "$to" --argstr vpcId        "$shared_vpc_id"
-        nixops set-args -d "$to" --argstr vpcCidr      "$shared_vpc_cidr"
-        nixops set-args -d "$to" --argstr routeTableId "$route_table_id"
-
-    elif [[ $mode == remote ]]; then
-        # Write IDs to files on server
-        echo "$shared_vpc_id" >| shared_vpc_id
-        echo "$shared_vpc_cidr" >| shared_vpc_cidr
-        echo "$route_table_id" >| route_table_id
-
-        nixops scp -d "$from" builder shared_vpc_id   /var/lib/nixops/shared_vpc_id
-        nixops scp -d "$from" builder shared_vpc_cidr /var/lib/nixops/shared_vpc_cidr
-        nixops scp -d "$from" builder route_table_id  /var/lib/nixops/route_table_id
-    fi
-
-elif [[ $do == read ]]; then
-    # Read existing IDs from files and write to deployment
-    nixops set-args -d "$to" --argstr vpcId        "$(cat /var/lib/nixops/shared_vpc_id)"
-    nixops set-args -d "$to" --argstr vpcCidr      "$(cat /var/lib/nixops/shared_vpc_cidr)"
-    nixops set-args -d "$to" --argstr routeTableId "$(cat /var/lib/nixops/route_table_id)"
-fi
+# Write IDs to local deployment
+nixops set-args -d "$to" --argstr vpcId        "$shared_vpc_id"
+nixops set-args -d "$to" --argstr vpcCidr      "$shared_vpc_cidr"
+nixops set-args -d "$to" --argstr routeTableId "$route_table_id"
