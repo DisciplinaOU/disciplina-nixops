5719d23c830bb4e13b8171db298cf18cdedd3999
Lars Jellema <lars.jellema@gmail.com>
2019-01-18 20:37:15 +0100

    bootstrap.sh: Cleanup and documentation

--- scripts/bootstrap.sh
+++ scripts/bootstrap.sh
@@ -6,21 +6,26 @@ set -euo pipefail
 # Used to select secrets from secretsmanager
 deployer_env="production"
 
+# Deployer will receive domain name "deployer.$deployer_domain"
+deployer_domain="example.net.disciplina.io"
+
 deployment_repo="https://github.com/DisciplinaOU/disciplina-nixops"
+NIXOPS_DEPLOYMENT="deployer"
+AWS_SHARED_CREDENTIALS_FILE="$HOME/.aws/credentials"
+
+# Do not change this. Use a different credential file if necessary.
+AWS_ACCESS_KEY_ID="default"
 nixops_home="/var/lib/nixops"
 
-export AWS_ACCESS_KEY_ID="default"
-export NIXOPS_DEPLOYMENT="deployer"
-
-[ -f "$HOME/.aws/credentials" ] || {
+[ -f "$AWS_SHARED_CREDENTIALS_FILE" ] || {
   echo "* No AWS credentials file. Creating it..."
-  mkdir -p "$HOME/.aws"
-  cat > "$HOME/.aws/credentials" <<-EOM
+  mkdir -p "$(dirname "$AWS_SHARED_CREDENTIALS_FILE")"
+  cat > "$AWS_SHARED_CREDENTIALS_FILE" <<-EOM
 	[default]
 	aws_access_key_id=...
 	aws_secret_access_key=...
 	EOM
-  echo '** Put your credentials into $HOME/.aws/credentials'
+  echo "** Put your credentials into $AWS_SHARED_CREDENTIALS_FILE"
   exit
 }
 
@@ -28,6 +33,7 @@ if ! nixops info >/dev/null 2>&1; then
   echo "* Creating the deployment..."
   nixops create deployments/deployer.nix
   nixops set-args --argstr env bootstrap
+  nixops set-args --argstr domain "$deployer_domain"
 else
   echo "! You already have a deployment called '$NIXOPS_DEPLOYMENT'."
   read -p "!! Press Enter to proceed with redeploying it..."
@@ -39,16 +45,14 @@ nixops deploy
 echo "* Moving everything to the deployer..."
 state=$(nixops export)
 cmd=""
-cmd+="mkdir -p '$nixops_home' && "
-cmd+="chmod g+rwxs '$nixops_home' && "
 cmd+="cd '$nixops_home' && "
 cmd+="sudo -u nixops git clone '$deployment_repo' disciplina-nixops && "
-cmd+="sudo -u nixops nixops import -d '$NIXOPS_DEPLOYMENT' && "
-cmd+="sudo -u nixops nixops modify ./disciplina-nixops/deployments/deployer.nix"
-cmd+="sudo -u nixops nixops set-args --argstr env '$deployer_env'"
-echo "$state" | nixops ssh disciplina-deployer "$cmd"
+cmd+="nixops import -d '$NIXOPS_DEPLOYMENT' && "
+cmd+="nixops modify disciplina-nixops/deployments/deployer.nix && "
+cmd+="nixops set-args --argstr env '$deployer_env'"
+echo "$state" | nixops ssh deployer "$cmd"
 
-public_ip=$(echo "$state" | jq -r '.[].resources["disciplina-deployer"].publicDnsName')
+public_ip=$(echo "$state" | jq -r '.[].resources["deployer"].publicDnsName')
 echo "* All done."
 
 echo "SSH into the new deployer at $public_ip"
