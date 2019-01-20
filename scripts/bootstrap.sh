04dad3000394326f7af884034e27ce4640fd2857
Kirill Elagin <kirelagin@gmail.com>
2019-01-21 02:59:33 +0300

    Aviod double nix-shell in bootstrap

--- scripts/bootstrap.sh
+++ scripts/bootstrap.sh
@@ -1,5 +1,4 @@
-#!/usr/bin/env nix-shell
-#!nix-shell -i bash -p bash jq nixops
+#!/usr/bin/env bash
 
 #
 # Usage: ./bootstrap.sh
--- shell.nix
+++ shell.nix
@@ -5,16 +5,11 @@ with pkgs;
 stdenv.mkDerivation {
   name = "disciplina-nixops";
   nativeBuildInputs = [
-    awscli
+    jq
     nixops
   ];
 
   NIX_PATH = lib.concatStringsSep ":" [
     "nixpkgs=${pkgs.path}"
-    "disciplina=https://github.com/DisciplinaOU/disciplina/archive/master.tar.gz"
-    "disciplina-faucet-frontend=https://github.com/DisciplinaOU/disciplina-faucet-frontend/archive/master.tar.gz"
-    "disciplina-explorer-frontend=https://github.com/DisciplinaOU/disciplina-explorer-frontend/archive/master.tar.gz"
-    "disciplina-validatorcv=https://github.com/DisciplinaOU/disciplina-validatorcv/archive/master.tar.gz"
-    "disciplina-nixops=${builtins.toString ./.}"
   ];
 }
